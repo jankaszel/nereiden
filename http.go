3040f6caed025a3fd889e83809c4f4034375fd02
jk <1777980+fallafeljan@users.noreply.github.com>
2018-08-13 14:37:43 +0200

    add query-based token auth

--- http.go
+++ http.go
@@ -9,11 +9,14 @@ func requireToken(token string) gin.HandlerFunc {
 	requiredHeader := "Token " + token
 
 	return func(c *gin.Context) {
-		if c.GetHeader("Authorization") != requiredHeader {
-			c.Header("WWW-Authenticate", "Token realm=\"Authorization Required\"")
-			c.AbortWithStatus(http.StatusUnauthorized)
-
+		if queryToken, exists := c.GetQuery("access_token"); exists && queryToken == token {
+			return
+		} else if c.GetHeader("Authorization") == requiredHeader {
 			return
 		}
+
+		c.Header("WWW-Authenticate", "Token realm=\"Authorization Required\"")
+		c.AbortWithStatus(http.StatusUnauthorized)
+
 	}
 }
